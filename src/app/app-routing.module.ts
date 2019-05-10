66dcf40f41b9e7bfa57716350ca7e9f616d9ded1
Nikolai <nk-west@hotmail.de>
2019-05-10 17:30:16 +0200

    Navigation zwischen Login und Register implementiert.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 ];
 
 @NgModule({
