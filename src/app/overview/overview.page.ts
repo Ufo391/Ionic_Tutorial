05ec856fc35c3738e7e658d401c636ed7bed6cc2
Nikolai <nk-west@hotmail.de>
2019-05-27 14:46:25 +0200

    Überblick-Page-GUI: Spielerauswahl und Größe Range eingebaut.

--- src/app/overview/overview.page.ts
+++ src/app/overview/overview.page.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.page.html',
-  styleUrls: ['./overview.page.scss'],
+  selector: "app-overview",
+  templateUrl: "./overview.page.html",
+  styleUrls: ["./overview.page.scss"]
 })
 export class OverviewPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }  
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
